7bfadf9355ef572acb7b31045d2c57c2d64445b8
Athil-TK <athulathul252003@gmail.com>
2026-01-26 17:15:15 +0530

    api files

--- src/service/allApi.js
+++ src/service/allApi.js
@@ -4,6 +4,33 @@ import { serverURL } from "./serverUrl";
 /////////////////////////////////////////  API CALL  //////////////////////////////////////////
 
 
+////////////////////////////////////////  categories  ////////////////////////////////
+//1.add categories to server API
 export const CategoriesAPI=async(reqBody)=>{
     return await commonAPI("POST",`${serverURL}/categories`,reqBody)
+}
+
+//2.get all categories from the server
+export const getallCategoriesAPI=async()=>{
+    return await commonAPI("GET",`${serverURL}/categories`)
+}
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////  FoodS Items  ////////////////////////////////
+
+//1.add food items to server API
+export const AddFoodItemsAPI=async(reqBody)=>{
+    return await commonAPI("POST",`${serverURL}/foods`,reqBody)
+}
+
+//2.get all categories from the server
+export const getallFoodItemsAPI=async()=>{
+    return await commonAPI("GET",`${serverURL}/foods`)
 }
