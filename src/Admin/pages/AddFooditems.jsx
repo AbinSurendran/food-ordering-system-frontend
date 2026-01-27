a9de3728848fdad04cefbbc0d74b5f56bb974106
Athil-TK <athulathul252003@gmail.com>
2026-01-27 21:15:36 +0530

    Modal for Edit and api for edit and delete update

--- src/Admin/pages/AddFooditems.jsx
+++ src/Admin/pages/AddFooditems.jsx
@@ -3,15 +3,26 @@ import AdminSlidebar from '../component/AdminSlidebar'
 import { RiDeleteBinLine } from "react-icons/ri";
 import { MdOutlineModeEdit } from "react-icons/md";
 import { IoCloseSharp } from "react-icons/io5";
-import { AddFoodItemsAPI, AddPopularDishesAPI, CategoriesAPI, getallCategoriesAPI, getallFoodItemsAPI, getallPopDishesAPI } from '../../service/allApi';
+import { AddFoodItemsAPI, AddPopularDishesAPI, CategoriesAPI, DeleteFoodItemsAPI, getAFoodItemsAPI, getallCategoriesAPI, getallFoodItemsAPI, getallPopDishesAPI, UpdateFoodItemsAPI } from '../../service/allApi';
+import { LogIn } from 'lucide-react';
 
 function AddFooditems() {
   //modal1 for add food items
   const [modalStatus, setmodalStatus] = useState(false)
   //modal2 for Categories and popular dish
   const [modal2Status, setmodal2Status] = useState(false)
+      //modal1 for Edit food items
+  const [editModal, seteditModal] = useState(false)
+      //modal1 for Edit PopDishes && category 
+  const [edit2Modal, setedit2Modal] = useState(false)
   //state foe search
   const [Search,setSearch]=useState("")
+    //state for store Edited Food items
+  const [EditFoodItems, setEditFoodItems] = useState([])
+      //state for store Edited Popular dishes 
+  const [EditPopDishes, setEditPopDishes] = useState([])
+        //state for store Edited Popular dishes 
+  const [EditCategory, setEditCategory] = useState([])
   //state for store categories
   const [categories, setcategories] = useState({
     id: "",
@@ -42,13 +53,15 @@ function AddFooditems() {
   const [gettAllFooditem, setgetAllFooditme] = useState([])
   //state for store Food items
   const [gettAllPopDishes, setgetAllPopdishes] = useState([])
-  console.log(gettAllcategories);
-  console.log(gettAllFooditem);
-  console.log(addFoods);
-  console.log(Search);
-  console.log(PopDishes);
-  console.log(gettAllPopDishes);
-  console.log(categories);
+  // console.log(gettAllcategories);
+  // console.log(gettAllFooditem);
+  // console.log(addFoods);
+  // console.log(Search);
+  // console.log(PopDishes);
+  // console.log(gettAllPopDishes);
+  // console.log(categories);
+  console.log(EditFoodItems);
+  
   //useEffect for get categories
   useEffect(()=>{
     getallcate()
@@ -187,6 +200,76 @@ function AddFooditems() {
     }
 
   }
+
+  //edit a Food Items fuction
+  const EditFood =async(id)=>{
+    try {
+      const result=await getAFoodItemsAPI(id)
+       if (result.status > 199 || result.status < 300) {
+        console.log(result);
+        setEditFoodItems(result.data)
+
+      }
+      else {
+        alert("Something Went worng get a food items")
+      }
+      
+      
+    } catch (error) {
+      console.log(error);
+      
+      
+    }
+  }
+
+
+   //Update a Food Items fuction
+  const UpadteFood =async()=>{
+    try {
+      const result=await UpdateFoodItemsAPI(EditFoodItems.id,EditFoodItems)
+       if (result.status > 199 || result.status < 300) {
+        console.log(result);
+        alert("Updated Food Items Succefully")
+        getallFoods()
+        seteditModal(!editModal)
+
+
+      }
+      else {
+        alert("Something Went worng Update a food item")
+      }
+      
+      
+    } catch (error) {
+      console.log(error);
+      
+      
+    }
+  }
+
+
+
+    //Delete a Food Items fuction
+  const DeleteFood =async(id)=>{
+    try {
+      const result=await DeleteFoodItemsAPI(id)
+       if (result.status > 199 || result.status < 300) {
+        console.log(result);
+        alert("Deleted Food Items Succefully")
+        getallFoods()  
+
+      }
+      else {
+        alert("Something Went worng Delete a food item")
+      }
+      
+      
+    } catch (error) {
+      console.log(error);
+      
+      
+    }
+  }
   
 
   
@@ -263,7 +346,7 @@ function AddFooditems() {
                         <td className='h-10 text-center text-sm align-middle p-2'></td>
                         <td className='h-10 text-center text-sm align-middle p-2'>{dishe.Dprice}</td>
                         <td className='h-10 text-center align-middle p-2 '>
-                          <button className='p-2 rounded-xl text-orange-500 border border-orange-500 hover:bg-orange-500 hover:text-white ms-1 me-3'><MdOutlineModeEdit className='text-1xl' /></button>
+                          <button onClick={()=>setedit2Modal(!edit2Modal)} className='p-2 rounded-xl text-orange-500 border border-orange-500 hover:bg-orange-500 hover:text-white ms-1 me-3'><MdOutlineModeEdit className='text-1xl' /></button>
                           <button className='p-2 rounded-xl bg-red-500 text-white'><RiDeleteBinLine className='text-1xl' /></button>
                         </td>
                       </tr>))}
@@ -299,8 +382,8 @@ function AddFooditems() {
                     <td className='h-20 text-center align-middle'>{items.category}</td>
                     <td className='h-20 text-center align-middle'>{items.price}</td>
                     <td className='h-20 text-center align-middle '>
-                      <button className='p-2 rounded-xl text-orange-500 border border-orange-500 hover:bg-orange-500 hover:text-white ms-1 me-3'><MdOutlineModeEdit className='text-xl' /></button>
-                      <button className='p-2 rounded-xl bg-red-500 text-white'><RiDeleteBinLine className='text-xl' /></button>
+                      <button onClick={()=>{seteditModal(!editModal),EditFood(items?.id)}} className='p-2 rounded-xl text-orange-500 border border-orange-500 hover:bg-orange-500 hover:text-white ms-1 me-3'><MdOutlineModeEdit className='text-xl' /></button>
+                      <button onClick={()=>DeleteFood(items?.id)}  className='p-2 rounded-xl bg-red-500 text-white'><RiDeleteBinLine className='text-xl' /></button>
                     </td>
                   </tr>))
                   }
@@ -371,6 +454,57 @@ function AddFooditems() {
           </div>
         </div>}
 
+        {/* Edit food items modal */}
+      {editModal &&
+        <div className='relative z-10 overflow-y-auto '>
+          <div className='fixed inset-0 bg-gray-500/75 overflow-y-auto '>
+            <div className='flex justify-center items-center min-h-screen py-8'>
+              <div className='bg-white rounded-2xl md:w-160 w-100 p-5  '>
+                <div className='bg-white text-black font-bold text-2xl rounded-xl flex justify-between items-center p-5 '>
+                  <h3>Edit Food Items</h3>
+                  <IoCloseSharp onClick={() => seteditModal(!editModal)} className='text-2xl cursor-pointer' />
+
+                </div>
+                <div className='flex flex-col justify-center  p-3'>
+                  <label className='font-bold text-gray-600' htmlFor="">Name</label>
+                  <input value={EditFoodItems.FName} onChange={(e) => setEditFoodItems({ ...EditFoodItems, FName: e.target.value })} className='h-12 rounded-xl border border-gray-300 p-5' type="text" placeholder='e.g : Normal Pizza' />
+                </div>
+                <div className='flex  justify-between  p-3  gap-2'>
+                  <div className='flex flex-col w-full'>
+                    <label className='font-bold text-gray-600' htmlFor="">Price ($)</label>
+                    <input value={EditFoodItems.price} onChange={(e) => setEditFoodItems({ ...EditFoodItems, price: e.target.value })} className='h-12 rounded-xl border border-gray-300 p-5' type="text" placeholder='10.10' />
+                  </div>
+                  <div className='flex flex-col w-full'>
+                    <label className='font-bold text-gray-600' htmlFor="">Categories</label>
+                    <select value={EditFoodItems.category} onChange={(e) => setEditFoodItems({ ...EditFoodItems, category: e.target.value })} className='h-12 rounded-xl border border-gray-300 text-gray-500 font-bold p-3' name="" id=""> 
+                        <option  selected >Selecte Category</option>
+                       {gettAllcategories&& gettAllcategories.map((categoray)=>(<>
+                        <option key={categoray.id} value={categoray.id}>{categoray.icon} {categoray.Name}</option>
+                        </>)) }
+                    </select>
+                  </div>
+                </div>
+                <div className='flex flex-col justify-center  p-3'>
+                  <label className='font-bold text-gray-600' htmlFor="">Image URL</label>
+                  <input value={EditFoodItems.image} onChange={(e) => setEditFoodItems({ ...EditFoodItems, image: e.target.value })} className='h-12 rounded-xl border border-gray-300 p-5' type="text" placeholder='http//....' />
+                </div>
+                <div className='flex flex-col justify-center  p-3'>
+                  <label className='font-bold text-gray-600' htmlFor="">Preparation Time</label>
+                  <input value={EditFoodItems.preparationTime} onChange={(e) => setEditFoodItems({ ...EditFoodItems, preparationTime: e.target.value })} className='h-12 rounded-xl border border-gray-300 p-5' type="text" placeholder='20-25 m' />
+                </div>
+                <div className='flex flex-col justify-center  p-3'>
+                  <label className='font-bold text-gray-600' htmlFor="">Description</label>
+                  <textarea value={EditFoodItems.description} onChange={(e) => setEditFoodItems({ ...EditFoodItems, description: e.target.value })} className='rounded-xl border border-gray-300 p-5' name="" id="" cols="3" rows="3" placeholder='Describe dish ... '></textarea>
+                </div>
+                <div className='flex justify-end p-3 gap-2'>
+                  <button onClick={() => seteditModal(!editModal)} className='border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white p-3 text-lg rounded-2xl'>Cancel</button>
+                  <button onClick={UpadteFood} className='bg-orange-600 text-white p-3 text-lg rounded-2xl'>Update Items</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>}
+
 
 
       {/* add food categories */}
@@ -403,6 +537,9 @@ function AddFooditems() {
                         <label className='font-bold text-gray-600' htmlFor="cn">Cate Name</label>
                         <input onChange={(e) => setcategories({ ...categories, Name: e.target.value })} id='cn' className='h-12 rounded-xl border border-gray-300 p-3' type="text" placeholder='    e.g : Pizza' />
                       </div>
+                    </div>
+                    <div className='flex justify-end mt-3 p-2'>
+                      <button onClick={Addcategary} className='bg-orange-600 text-white p-3 text-lg rounded-2xl'>Add Cate</button>
                     </div>
 
                   </div>
@@ -456,12 +593,107 @@ function AddFooditems() {
                 <div className='flex justify-end p-3 gap-2'>
                   <button onClick={() => setmodal2Status(!modal2Status)} className='border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white p-3 text-lg rounded-2xl'>Cancel</button>
                   <button onClick={AddPopDishes}  className='bg-orange-600 text-white p-3 text-lg rounded-2xl'>Add Dishes</button>
-                  <button onClick={Addcategary} className='bg-orange-600 text-white p-3 text-lg rounded-2xl'>Add Cate</button>
+                  
                 </div>
               </div>
             </div>
           </div>
         </div>}
+
+
+      {/* Edit food categories */}
+      {edit2Modal &&
+        <div className='relative z-10 overflow-y-auto '>
+          <div className='fixed inset-0 bg-gray-500/75 overflow-y-auto '>
+            <div className='flex justify-center items-center min-h-screen py-8'>
+              <div className='bg-white rounded-2xl md:w-160 w-100 p-5  '>
+                <div className='bg-white text-black font-bold text-2xl rounded-xl flex justify-between items-center p-5 '>
+                  <h3>Edit Categories List && Popular Dishes</h3>
+                  <IoCloseSharp onClick={() => setedit2Modal(!edit2Modal)} className='text-2xl cursor-pointer' />
+
+                </div>
+
+                <div className='flex flex-col gap-3'>
+                  <div className='  p-3 '>
+                    <h2 className='text-gray-600 text-xl font-bold'>Categories List</h2>
+                    <div className='flex  justify-center  p-3 gap-3 '>
+                      <div className='flex flex-col w-full'>
+                        <label className='font-bold text-gray-600 ' htmlFor="cid">Cate ID</label>
+                        <input onChange={(e) => setcategories({ ...categories, id: e.target.value })} id='cid' className='h-12 rounded-xl border border-gray-300 p-3' type="text" placeholder='    e.g : Id/Name' />
+                      </div>
+                      <div className='flex flex-col w-full'>
+                        <label className='font-bold text-gray-600' htmlFor="ci"> Cate Icon</label>
+                        <input onChange={(e) => setcategories({ ...categories, icon: e.target.value })} id='ci' className='h-12 rounded-xl border border-gray-300 p-3' type="text" placeholder='    e.g : 🍕' />
+                      </div>
+                    </div>
+                    <div className='flex  justify-center  p-3  '>
+                      <div className='flex flex-col w-full'>
+                        <label className='font-bold text-gray-600' htmlFor="cn">Cate Name</label>
+                        <input onChange={(e) => setcategories({ ...categories, Name: e.target.value })} id='cn' className='h-12 rounded-xl border border-gray-300 p-3' type="text" placeholder='    e.g : Pizza' />
+                      </div>
+                    </div>
+                    <div className='flex justify-end mt-3 p-2'>
+                        <button onClick={Addcategary} className='bg-orange-600 text-white p-3 text-lg rounded-2xl'>Update Cate</button>
+                    </div>
+                     
+
+                  </div>
+                  <div className='border-b'></div>
+                  <div className='  p-3'>
+                    <h2 className='text-gray-600 text-xl font-bold'>Popular Dishes</h2>
+                    <div className='flex  justify-center  p-3 gap-3 '>
+                      <div className='flex flex-col w-full'>
+                        <label className='font-bold text-gray-600' htmlFor="cid">Name</label>
+                        <input onChange={(e) => setPopDishees({ ...PopDishes, DName: e.target.value })} id='cid' className='h-12 rounded-xl border border-gray-300 p-3' type="text" placeholder='    e.g : Name' />
+                      </div>
+                      <div className='flex flex-col w-full'>
+                        <label className='font-bold text-gray-600' htmlFor="ci">Price($)</label>
+                        <input onChange={(e) => setPopDishees({ ...PopDishes, Dprice: e.target.value })} id='ci' className='h-12 rounded-xl border border-gray-300 p-3 ' type="text" placeholder='    e.g : $ 12.1' />
+                      </div>
+                    </div>
+                    <div className='flex  justify-center  p-3  '>
+                      <div className='flex flex-col w-full'>
+                        <label className='font-bold text-gray-600' htmlFor="cn">Image URL</label>
+                        <input onChange={(e) => setPopDishees({ ...PopDishes, Dimage: e.target.value })} id='cn' className='h-12 rounded-xl border border-gray-300 p-3' type="text" placeholder='    e.g : http//...' />
+                      </div>
+                    </div>
+                    <div className='flex  justify-between  p-3  gap-2'>
+                  <div className='flex flex-col w-full'>
+                   <label className='font-bold text-gray-600' htmlFor="cn">Preparation Time</label>
+                        <input onChange={(e) => setPopDishees({ ...PopDishes, DpreparationTime: e.target.value })} id='cn' className='h-12 rounded-xl border border-gray-300 p-3' type="text" placeholder='    e.g : 20-25 min' />
+                  </div>
+                  <div className='flex flex-col w-full'>
+                    <label className='font-bold text-gray-600' htmlFor="">Categories</label>
+                    <select onChange={(e) => setPopDishees({ ...PopDishes, Dcategory: e.target.value })} className='h-12 rounded-xl border border-gray-300 text-gray-500 font-bold p-3' name="" id=""> 
+                        <option  selected >Selecte Category</option>
+                       {gettAllcategories&& gettAllcategories.map((categoray)=>(<>
+                        <option key={categoray.id} value={categoray.id}>{categoray.icon} {categoray.Name}</option>
+                        </>)) }
+                    </select>
+                  </div>
+                </div>
+                    <div className='flex  justify-center  p-3  '>
+                      <div className='flex flex-col w-full'>
+                        
+                      </div>
+                    </div>
+                    <div className='flex flex-col justify-center  p-3'>
+                  <label className='font-bold text-gray-600' htmlFor="">Description</label>
+                  <textarea onChange={(e) => setPopDishees({ ...PopDishes, Ddescription: e.target.value })} className='rounded-xl border border-gray-300 p-5' name="" id="" cols="3" rows="3" placeholder='Describe dish ... '></textarea>
+                </div>
+
+                  </div>
+
+                </div>
+                <div className='flex justify-end p-3 gap-2'>
+                  <button onClick={() => setedit2Modal(!edit2Modal)} className='border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white p-3 text-lg rounded-2xl'>Cancel</button>
+                  <button onClick={AddPopDishes}  className='bg-orange-600 text-white p-3 text-lg rounded-2xl'>Update Dishes</button>
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>}  
 
 
 
--- src/service/allApi.js
+++ src/service/allApi.js
@@ -30,9 +30,24 @@ export const AddFoodItemsAPI=async(reqBody)=>{
     return await commonAPI("POST",`${serverURL}/foods`,reqBody)
 }
 
-//2.get all categories from the server
+//2.get all Fooditems from the server
 export const getallFoodItemsAPI=async()=>{
     return await commonAPI("GET",`${serverURL}/foods`)
+}
+
+//3.get a Food items from  server for edit
+export const getAFoodItemsAPI=async(id)=>{
+    return await commonAPI("GET",`${serverURL}/foods/${id}`)
+}
+
+//4.Upadte a Food items to  server for edited data
+export const UpdateFoodItemsAPI=async(id,reqBody)=>{
+    return await commonAPI("PUT",`${serverURL}/foods/${id}`,reqBody)
+}
+
+//4.Delete a Food items from  server 
+export const DeleteFoodItemsAPI=async(id)=>{
+    return await commonAPI("DELETE",`${serverURL}/foods/${id}`)
 }
 
 
